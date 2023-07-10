93141059b5a035c7830294a32bd0b213dbcbb104
Đào Duy Tùng <duytung.developer@gmail.com>
2023-07-10 10:23:49 +0700

    Setup project

---
+++ org/apps/online-courses-be/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'online-courses-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/online-courses-be',
+};
